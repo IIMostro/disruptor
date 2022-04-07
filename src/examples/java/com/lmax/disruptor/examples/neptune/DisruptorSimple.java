06dac9c2aa8b8637d8b4919d0795d9b04137d73e
li.bowei <1306475495@qq.com>
2022-04-07 14:26:47 +0800

    添加几个示例，新版的删除了WorkProcess,也没有了handleWithWorkProcess

--- src/examples/java/com/lmax/disruptor/examples/neptune/DisruptorSimple.java
+++ src/examples/java/com/lmax/disruptor/examples/neptune/DisruptorSimple.java
@@ -1,15 +1,21 @@
 package com.lmax.disruptor.examples.neptune;
 
 import com.lmax.disruptor.dsl.Disruptor;
-
-import java.util.concurrent.TimeUnit;
+import com.lmax.disruptor.dsl.EventHandlerGroup;
 
 /**
  * @author li.bowei
  */
-public class DisruptorSimple {
+public class DisruptorSimple
+{
 
     public static void main(String[] args) throws Exception
+    {
+        factory();
+    }
+
+
+    public void simple()
     {
         Disruptor<OrderEvent> disruptor = DisruptorConfiguration.getInstance();
         disruptor.handleEventsWith(new MessageEventHandler());
@@ -19,7 +25,68 @@ public class DisruptorSimple {
             final int fi = i;
             disruptor.publishEvent(((event, sequence) -> event.setId(fi)));
         }
+        disruptor.shutdown();
+    }
 
-        TimeUnit.SECONDS.sleep(1);
+    public static void multiMessageHandler()
+    {
+        Disruptor<OrderEvent> disruptor = DisruptorConfiguration.getInstance();
+        for (int i = 0; i < 5; i++)
+        {
+            disruptor.handleEventsWith(new MessageEventHandler());
+        }
+        disruptor.start();
+        for (int i = 0; i < 10; i++)
+        {
+            final int fi = i;
+            disruptor.publishEvent(((event, sequence) -> event.setId(fi)));
+        }
+        disruptor.shutdown();
+    }
+
+    public static void arrayMessageHandler()
+    {
+        Disruptor<OrderEvent> disruptor = DisruptorConfiguration.getInstance();
+        MessageEventHandler[] handlers = new MessageEventHandler[5];
+        for (int i = 0; i < 5; i++)
+        {
+            handlers[i] = new MessageEventHandler();
+        }
+        disruptor.handleEventsWith(handlers);
+        disruptor.start();
+        for (int i = 0; i < 10; i++)
+        {
+            final int fi = i;
+            disruptor.publishEvent(((event, sequence) -> event.setId(fi)));
+        }
+        disruptor.shutdown();
+    }
+
+    public static void groupMessageHandler()
+    {
+        Disruptor<OrderEvent> disruptor = DisruptorConfiguration.getInstance();
+
+        EventHandlerGroup<OrderEvent> group = disruptor.handleEventsWith(new MessageEventHandler(), new MessageEventHandler());
+
+        disruptor.start();
+        for (int i = 0; i < 10; i++)
+        {
+            final int fi = i;
+            disruptor.publishEvent(((event, sequence) -> event.setId(fi)));
+        }
+        disruptor.shutdown();
+    }
+
+    public static void factory()
+    {
+        Disruptor<OrderEvent> disruptor = DisruptorConfiguration.getInstance();
+        disruptor.handleEventsWith(new MultiMessageEventProcessorFactory());
+        disruptor.start();
+        for (int i = 0; i < 10; i++)
+        {
+            final int fi = i;
+            disruptor.publishEvent(((event, sequence) -> event.setId(fi)));
+        }
+        disruptor.shutdown();
     }
 }
